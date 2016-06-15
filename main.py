05a21c7d8097585c50f57aa99f74aedd2c9ab6b3
Kiker <rafalrutyna00@gmail.com>
2016-06-15 09:32:53 +0200

    Dodanie bootstrapa i menu

--- main.py
+++ main.py
@@ -3,23 +3,36 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 from contextlib import closing
 
 # Konfiguracja
-
 DATABASE = 'baza_danych/baza.db'
-DEBUG=True
+DEBUG = True
 SECRET_KEY = 'development key'
-
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-def polacz_z_baza():
+
+def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-def inicjuj_baze():
-    with closing(polacz_z_baza()) as db:
+
+def init_db():
+    with closing(connect_db()) as db:
         with app.open_resource('schemat_bazy.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
+
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
 
 @app.route('/')
 def index():
@@ -30,6 +43,6 @@ def index():
 def student():
     return 'student main site !'
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
